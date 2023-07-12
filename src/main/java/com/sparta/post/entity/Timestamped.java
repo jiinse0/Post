23c7aa8e92b6de68d7de15be3627bca6109e7f1d
jiinse0 <jee_ji@naver.com>
2023-07-12 10:21:01 +0900

    [feat] 어노테이션 변경

--- src/main/java/com/sparta/post/entity/Timestamped.java
+++ src/main/java/com/sparta/post/entity/Timestamped.java
@@ -3,6 +3,7 @@ package com.sparta.post.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -27,7 +28,7 @@ public abstract class Timestamped {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
-    @CreatedDate
+    @LastModifiedDate
     @Column(name = "last_modified_date")
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime modifiedAt;
